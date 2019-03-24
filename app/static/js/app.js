82d46b39951f6201a89ee73e321c0d70f422f13d
RachelleT <rache_trotman@hotmail.com>
2019-03-24 13:53:47 -0500

    Updating Search

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -26,21 +26,21 @@ Vue.component('app-header', {
 
 Vue.component('news-list', {
     template: `
-        <div class="news">
+    <div class="form-inline d-flex justify-content-center">
+		<div class="form-group mx-sm-3 mb-2">
+			<label class="sr-only" for="search">Search</label>
+			<input type="search" name="search" v-model="searchTerm"
+			id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+			<button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+		</div>
+	</div>
+      <div class="news">
 		<h2>News</h2>
 		<ul class="news__list">
 		<li v-for="article in articles" class="news__item"> {{ article.title }} 
 		<img :src="article.urlToImage">
 		 {{article.description}} </li>
 		</ul>
-	</div>
-	<div class="form-inline d-flex justify-content-center">
-		<div class="form-group mx-sm-3 mb-2">
-			<label class="sr-only" for="search">Search</label>
-			<input type="search" name="search" v-model="searchTerm"
-			id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
-			<p>You are searching for {{ searchTerm }}</p>
-		</div>
 	</div>
     		`,
     created: function() {
@@ -55,6 +55,19 @@ Vue.component('news-list', {
 	},
 	data: function() {
 		return { articles: [], searchTerm: '' }
+	},
+	methods: {
+	searchNews: function() {
+		let self = this;
+		fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en&apiKey=<your-api-key>')
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data) {
+		console.log(data);
+		self.articles = data.articles;
+		});
+	}
 	}
 });
 
